343fbc6f8a753b2e2c324e8c7732b214b4428a89
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-06-24 19:40:09 +0700

    feat: CRD of database using sql

--- src/backend/app/db.js
+++ src/backend/app/db.js
@@ -1,3 +1,37 @@
-const db = require('./db.config')
+const conn = require('./db.config')
+const algo = require('./algorithms');
 
-const 
+const create = async (types, further, input, output, ratio) => {
+    try{
+        const [rows, fields] = await conn.promise().query('INSERT INTO History (types, further, input, output, ratio) VALUES (?, ?, ?, ?, ?)', [types, further, input, output, ratio]);
+    } catch (err) {
+        throw err;
+    }
+}
+
+const read = async () => {
+    try{
+        const [rows, fields] = await conn.promise().query('SELECT * FROM History');
+        return rows;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const remove = async (id) => {
+    try{
+        if (id == null){
+            const [rows, fields] = await conn.promise().query('DELETE FROM History');
+        } else {
+            const [rows, fields] = await conn.promise().query('DELETE FROM History WHERE id = ?', [id]);
+        }
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {
+    create,
+    read,
+    remove
+}
